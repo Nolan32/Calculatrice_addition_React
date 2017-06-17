e1e9258c9d2006917f75e9197bb037ba3e1760ac
nolan32 <jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.frgit config --global user.email jordy.duces@hotmail.fr>
2017-06-17 20:13:33 +0200

    ajout de la fonctionnalité addition

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Addition from'./component/Addition';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Addition/>
+      </div>  
+    )
+  }
+}
+
+export default App;
---
+++ src/component/Addition.js
@@ -0,0 +1,65 @@
+import React,{Component} from 'react';
+import Input from './Input';
+import Button from'./Button';
+
+export default class Addition extends Component{
+	constructor(){
+		super();
+		this.state={
+			addition : {left_number : 0,right_number : 0},
+			resultat:0
+		}
+		this._getResult = this._getResult.bind(this);
+		this._getInputValue=this._getInputValue.bind(this);
+	}
+
+	_object_ToArray(){
+		this.setState.addition= Object.keys(this.state.addition).map((key) => parseFloat(this.state.addition[key]));
+	}
+
+	_addition(){
+		this.setState({
+			result:this.setState.addition.reduce((a,b)=>a+b)
+		})
+	}
+
+	_getResult(){
+		this._object_ToArray.call(this);
+		this._addition.call(this);
+	}
+
+	_getInputValue(e){
+		let state=this.state.addition;
+		state[e.target.name]= e.target.value;
+		this.setState({
+			addition:state
+		})
+	}
+
+	render(){
+		return(
+			<div>
+				<div className='ui four column centered grid'>
+					<div className='column'>
+						<h2 className='centered-text'>Calculatrice</h2>
+						<span className='content-result  centered-text'>Resultat:
+						<span className='result'> {this.state.result}</span></span>
+						
+						<Input
+						name='left_number'
+						type='number'
+						action={this._getInputValue}
+						/>
+						<span className='addition centered-text'> + </span>
+						<Input
+						name='right_number'
+						type='number'
+						action={this._getInputValue}
+						/>
+						<Button action={this._getResult}/>
+					</div>
+				</div>
+			</div>			
+		)
+	}
+}
